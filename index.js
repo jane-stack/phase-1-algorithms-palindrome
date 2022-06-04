c0070d01d42c716d0650c76e1aac6856d51c1dad
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2022-06-03 18:50:31 -0600

    commit tree d98913d2f3fa4375a8284caeef3860b80dd42d03

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+  // let splitString = word.split('');
+  // let reverseArray = splitString.reverse();
+  // let joinArray = reverseArray.join('');
+  // return joinArray;
+
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word.split('').reverse().join('') == word) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+isPalindrome('abba'); //=> true
+isPalindrome('racecar'); //=> true
+isPalindrome('a'); //=> true
+isPalindrome('robot'); //=> false
+isPalindrome('ab'); //=> false
+
 /* 
-  Add your pseudocode here
+  reverse word,
+  return true if match
+  return false if no match
 */
 
 /*
   Add written explanation of your solution here
+  To reverse the word we must first split the word, then reverse it, and rejoin it.
+  We are able to give it a true/false result by using the if, else statements.
 */
 
 // You can run `node index.js` to view these console logs
